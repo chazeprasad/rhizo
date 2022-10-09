5f23f75aceba1ae259b042b4c51634cae1572af0
‘Prasad <chazeprasad@gmail.com>
2022-10-09 21:55:13 +0530

    updates seed

--- packages/seed/src/index.js
+++ packages/seed/src/index.js
@@ -1,44 +1,21 @@
 import * as React from 'react'
 
-export const SeedContext = React.createContext({})
+const initialState = {}
+
+const SeedContext = React.createContext(initialState)
+
 if (process.env.NODE_ENV !== 'production') {
   SeedContext.displayName = 'RhizoSeedContext'
 }
 
 export const useSeed = () => React.useContext(SeedContext)
 
-const getSeed = (outerSeed, seed) => {
-  if (typeof seed === 'function') {
-    const mergedSeed = seed(outerSeed)
-    if (
-      process.env.NODE_ENV !== 'production' &&
-      (mergedSeed == null ||
-        typeof mergedSeed !== 'object' ||
-        Array.isArray(mergedSeed))
-    ) {
-      throw new Error(
-        '[SeedProvider] Please return an object from your seed function, i.e. seed={() => ({})}!'
-      )
-    }
-    return mergedSeed
-  }
-  if (
-    process.env.NODE_ENV !== 'production' &&
-    (seed == null || typeof seed !== 'object' || Array.isArray(seed))
-  ) {
-    throw new Error('[SeedProvider] Please make your seed prop a plain object')
-  }
+const SeedProvider = ({ config, children }) => {
+  const { Provider } = SeedContext
 
-  return { ...outerSeed, ...seed }
+  const [seed, setSeed] = React.useState(config)
+
+  return <Provider value={{ seed }}>{children}</Provider>
 }
 
-export const SeedProvider = (props) => {
-  let seed = React.useContext(SeedContext)
-
-  if (props.seed !== seed) {
-    seed = createCacheWithSeed(seed)(props.seed)
-  }
-  return (
-    <SeedContext.Provider value={seed}>{props.children}</SeedContext.Provider>
-  )
-}
+export { SeedProvider, SeedContext }
